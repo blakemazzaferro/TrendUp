eb80eaddd5c488bd30476d9d584cead5bf4836de
Blake Mazzaferro <blake.mazzaferro@gmail.com>
2019-04-24 17:27:26 -0400

    Initial Commit

---
+++ TrendUp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrendUp
+//
+//  Created by Blake Mazzaferro on 4/24/19.
+//  Copyright © 2019 Blake Mazzaferro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
